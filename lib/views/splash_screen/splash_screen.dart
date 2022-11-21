a5015ce764fc95a497dc99d7df20f276d3a484e8
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-11-21 11:55:03 +0600

    Checked user already login or not

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:e_mart_app/consts/consts.dart';
+import 'package:e_mart_app/views/screens/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -10,17 +12,24 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  
-  changeScreen(){
-    Future.delayed(Duration(seconds: 3),(){
-      Get.offAll(()=> LoginScreen());
+  changeScreen() {
+    Future.delayed(Duration(seconds: 3), () {
+      firebaseAuth.authStateChanges().listen((User? user) {
+        if (user == null && mounted) {
+          Get.offAll(() => LoginScreen());
+        } else {
+          Get.offAll(() => Home());
+        }
+      });
     });
   }
+
   @override
   void initState() {
     super.initState();
     changeScreen();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +37,13 @@ class _SplashScreenState extends State<SplashScreen> {
       body: Center(
         child: Column(
           children: [
-            Align(alignment: Alignment.topLeft,child: Image.asset(icSplashBg,width: 300,),),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Image.asset(
+                icSplashBg,
+                width: 300,
+              ),
+            ),
             20.heightBox,
             appLogoWidget(),
             10.heightBox,
