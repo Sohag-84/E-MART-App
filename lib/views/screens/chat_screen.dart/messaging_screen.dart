524de5b2de954d95addad092a254cfccf6962c9d
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-11-28 02:27:56 +0600

    show user last message and chating with seller

--- lib/views/screens/chat_screen.dart/messaging_screen.dart
+++ lib/views/screens/chat_screen.dart/messaging_screen.dart
@@ -3,6 +3,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_mart_app/consts/consts.dart';
 import 'package:e_mart_app/services/firesoter_services.dart';
+import 'package:e_mart_app/views/screens/chat_screen.dart/chat_screen.dart';
 import 'package:e_mart_app/widgets/loading_indicator.dart';
 
 class MessagesScreen extends StatelessWidget {
@@ -29,7 +30,47 @@ class MessagesScreen extends StatelessWidget {
                 .color(darkFontGrey)
                 .makeCentered();
           } else {
-            return Container();
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        var data = snapshot.data!.docs[index];
+                        return Card(
+                          child: ListTile(
+                            onTap: () {
+                              Get.to(
+                                () => ChatScreen(),
+                                arguments: [
+                                  data['friend_name'],
+                                  data['toId'],
+                                ],
+                              );
+                            },
+                            leading: CircleAvatar(
+                              backgroundColor: redColor,
+                              child: Icon(
+                                Icons.person,
+                                color: whiteColor,
+                              ),
+                            ),
+                            title: "${data['friend_name']}"
+                                .text
+                                .fontFamily(semibold)
+                                .color(darkFontGrey)
+                                .make(),
+                            subtitle: "${data['last_msg']}".text.make(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
           }
         },
       ),
